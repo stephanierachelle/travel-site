64a66003c72bd46342fc42f4be953e26de3761cd
Steph <stephaniergreene94@gmail.com>
2019-03-23 14:24:26 +1100

    Completed lazy loading function

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -4,6 +4,7 @@ import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
 constructor() {
+    this.lazyImages = $(".lazyload");
     this.siteheader = $(".site-header");
     this.headerTriggerElement = $(".large-hero__title");
     this.createHeaderWaypoint();
@@ -11,6 +12,13 @@ constructor() {
     this.headerLinks = $(".primary-nav a")
     this.createPageSectionWaypoints();
     this.addSmoothScrooling();
+    this.refreshWayPoints();
+}
+
+refreshWayPoints() {
+    this.lazyImages.on('load', function() {
+        Waypoint.refreshAll();
+    })
 }
 
 addSmoothScrooling() {
