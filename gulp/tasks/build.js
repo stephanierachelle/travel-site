c964a9049f532a4205cc5bc8c6fda435af581659
Steph <stephaniergreene94@gmail.com>
2019-03-23 18:29:05 +1100

    Completed gulp build task 2

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -8,39 +8,38 @@ uglify = require('gulp-uglify'),
 browserSync = require('browser-sync').create();
 
 gulp.task('previewDist', function() {
-  browserSync.init({
-    notify: false,
-    server: {
-      baseDir: "docs"
-    }
-  });
-});
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "docs"
+        }
+    });
+})
 
 gulp.task('deleteDistFolder', ['icons'], function() {
-  return del("./docs");
+    return del("./docs");
 });
 
 gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
-  var pathsToCopy = [
-    './app/**/*',
-    '!./app/index.html',
-    '!./app/assets/images/**',
-    '!./app/assets/styles/**',
-    '!./app/assets/scripts/**',
-    '!./app/temp',
-    '!./app/temp/**'
-  ]
-
-  return gulp.src(pathsToCopy)
+    var pathsToCopy = [
+        './app/**/*',
+        '!./app/index.html',
+        '!./app/assets/images/**',
+        '!./app/assets/styles/**',
+        '!./app/assets/scripts/**',
+        '!./app/temp/',
+        '!./app/temp/**'
+    ]
+    return gulp.src(pathsToCopy)
     .pipe(gulp.dest("./docs"));
 });
 
 gulp.task('optimizeImages', ['deleteDistFolder'], function() {
-  return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
+    return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
     .pipe(imagemin({
-      progressive: true,
-      interlaced: true,
-      multipass: true
+        progessive: true,
+        interlaced: true,
+        multipass: true
     }))
     .pipe(gulp.dest("./docs/assets/images"));
 });
@@ -50,11 +49,12 @@ gulp.task('useminTrigger', ['deleteDistFolder'], function() {
 });
 
 gulp.task('usemin', ['styles', 'scripts'], function() {
-  return gulp.src("./app/index.html")
+    return gulp.src("./app/index.html")
     .pipe(usemin({
-      css: [function() {return rev()}, function() {return cssnano()}],
-      js: [function() {return rev()}, function() {return uglify()}]
+        css: [function() {return rev()}, function() {return cssnano()}],
+        js: [function() {return rev()}, function() {return uglify()}]
     }))
+
     .pipe(gulp.dest("./docs"));
 });
 
